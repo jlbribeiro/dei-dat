25327b866df08ea2da9020b5df790acc61391438
José Ribeiro <jlbribeiro@gmail.com>
2012-03-24 04:25:58 +0000

    [report/src] ex. 1 complete

--- tp2/src/ex1.m
+++ tp2/src/ex1.m
@@ -20,8 +20,6 @@ a = [1 -2.3 1.74 -0.432 0 0];
 zeroes = roots(b);
 poles = roots(a);
 
-% [zeroes, poles, ~] = tf2zpk(b, a);
-
 fprintf('Ex. 1.2.1\n\n');
 fprintf('         0.3137 z^2 - 0.1537\n');
 fprintf('-------------------------------------\n');
@@ -128,14 +126,41 @@ title(sprintf(titl_, 'in'));
 legend('y[n] unit step response using U(z) and iztrans', 'y[n] unit step response using dstep');
 
 %% Ex. 1.2.7
-LLIM = 0;
-RLIM = 70;
-
-syms z; syms n;
+syms z;
 H = (0.3137 * z^2 - 0.1537) / (z^5 - 2.3 * z^4 + 1.74 * z^3 - 0.432 * z^2);
 
 fprintf('Ex. 1.2.7\n');
-x_expr = input('Insert an expression for x[n] [e.g.: 4 * (heaviside(n - 3) - heaviside(n - 9))]: ');
+x_expr = input('Insert an expression for x[n] [ENTER for: 4 * (heaviside(n - 3) - heaviside(n - 9))]: ', 's');
+if isempty(x_expr)
+	x_expr = '4 * (heaviside(n - 3) - heaviside(n - 9))';
+end;
+
+x = sym(x_expr);
+
+X = ztrans(x);
+
+Y = X * H;
+y = iztrans(Y);
+
+fprintf(sprintf('The y[n] system''s response to %s:\n', char(x_expr))); pretty(y);
+
+%% Ex. 1.2.8
+LLIM = 0;
+RLIM = 70;
+
+b = [0 0 0 0.3137 0 -0.1537];
+a = [1 -2.3 1.74 -0.432 0 0];
+
+syms z;
+syms n;
+
+H = (0.3137 * z^2 - 0.1537) / (z^5 - 2.3 * z^4 + 1.74 * z^3 - 0.432 * z^2);
+
+fprintf('Ex. 1.2.8\n');
+x_expr = input('Insert an expression for x[n] [ENTER for: 4 * (heaviside(n - 3) - heaviside(n - 9))]: ', 's');
+if isempty(x_expr)
+	x_expr = '4 * (heaviside(n - 3) - heaviside(n - 9))';
+end;
 
 x = sym(x_expr);
 
@@ -146,10 +171,42 @@ y = iztrans(Y);
 
 n = LLIM:RLIM;
 
-y_n = subs(y, 'n', n);
-titl_ = sprintf('Ex. 1.2.7: y[n] response to the specific input (%s), n %%s [0, 70]', char(x_expr));
-figure('Name', sprintf(titl_, '∈'));
-stairs(n, y_n);
-title(sprintf(titl_, 'in'));
+x_n = double(subs(x, 'n', n(1:RLIM)+1));
 
-fprintf(sprintf('The y[n] system''s response to %s:\n', char(x_expr))); pretty(y);
+y1_n = subs(y, 'n', n);
+y2_n = filter(b, a, x_n);
+y3_n = dlsim(b, a, x_n);
+
+titl_ = sprintf('Ex. 1.2.8: y[n] response to the specific input (%s) using iztrans over convolution, filter and dlsim, n %%s [0, 70]', char(x_expr));
+figure('Name', sprintf(titl_, '∈'));
+hold all;
+stairs(n, y1_n);
+plot(y2_n, 'ro');
+plot(y3_n, 'g+');
+hold off;
+title(sprintf(titl_, 'in'));
+legend('y[n] response to the specific input using iztrans over convolution', 'y[n] response to the specific input using filter', 'y[n] response to the specific input using dlsim');
+
+%% Ex. 1.2.9
+b = [0 0 0 0.3137 0 -0.1537];
+a = [1 -2.3 1.74 -0.432 0 0];
+
+w = linspace(0, pi, 1000);
+H_w = freqz(b, a, w);
+
+titl_ = 'y[n] frequency response in %s, %s %s [0, %s] rad';
+figure('Name', sprintf('Ex. 1.2.9: %s', sprintf(titl_, 'amplitude (dB) and phase (deg)', 'Ω', '∈', 'π')));
+subplot(2, 1, 1);
+plot(db(abs(H_w)), 'b');
+title(sprintf(titl_, 'amplitude (dB)', 'w', 'in', 'pi'));
+
+subplot(2, 1, 2);
+plot(unwrap(rad2deg(angle(H_w))), 'b');
+title(sprintf(titl_, 'phase (deg)', 'w', 'in', 'pi'));
+
+%% Ex. 1.2.10
+b = [0 0 0 0.3137 0 -0.1537];
+a = [1 -2.3 1.74 -0.432 0 0];
+
+gain = ddcgain(b, a);
+fprintf('Ex. 1.2.10\nThe system''s gain is %.3f.\n', gain);
