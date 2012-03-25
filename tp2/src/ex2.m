9fec719b2027487d5c0fb4a51db53d9be9f62dcb
José Ribeiro <jlbribeiro@gmail.com>
2012-03-25 06:33:55 +0100

    [report] up to ex. 2.2.4; [src] up to ex. 2.2.4

--- tp2/src/ex2.m
+++ tp2/src/ex2.m
@@ -21,7 +21,7 @@ end
 m_max = 100;
 m_max_i = [0, 1, 3, 5, 10, 50];
 
-ex214_216(t_, x_t, T0, m_max, m_max_i);
+ex214_216(t_, x_t, T0, m_max, m_max_i, 'Ex. 2.1.4: ');
 
 %% Ex. 2.2.
 %% Ex. 2.2.1
@@ -32,7 +32,7 @@ x_t = ex213_square(t_);
 m_max = 100;
 m_max_i = [0, 1, 3, 5, 10, 50];
 
-ex214_216(t_, x_t, T0, m_max, m_max_i);
+ex214_216(t_, x_t, T0, m_max, m_max_i, 'Ex. 2.2.1: ');
 
 %% Ex. 2.2.2
 T0 = 2 * pi;
@@ -42,26 +42,38 @@ x_t = ex213_sawtooth(t_, T0);
 m_max = 100;
 m_max_i = [0, 1, 3, 5, 10, 50];
 
-ex214_216(t_, x_t, T0, m_max, m_max_i);
+ex214_216(t_, x_t, T0, m_max, m_max_i, 'Ex. 2.2.2: ');
 
 %% Ex. 2.2.3
-T0 = 2 * pi;
-t_ = linspace(0, T0, 500);
+% x_t	= 1 + 2 * sin(12 * pi * t_ + pi/4) .* cos(21 * pi * t_)
+%		= cos(0) + cos(33 * pi * t_ + 7 * pi / 4) + cos(9 * pi * t_ + pi / 4)
+%
+% W0	= 3 * pi
+% T0	= 2 * pi / W0 = 2 / 3;
 
-% x_t = 1 + 2 * sin(12 * pi * t_ + pi/4) .* cos(21 * pi * t_);
-x_t = 1 + cos(33 * pi * t_ + 7 * pi / 4) + cos(9 * pi * t_ + pi / 4);
+T0 = 2 / 3;
+
+t_ = linspace(0, T0, 500);
+x_t = cos(0) + cos(33 * pi * t_ + 7 * pi / 4) + cos(9 * pi * t_ + pi / 4);
 
 m_max = 100;
 m_max_i = [0, 1, 3, 5, 10, 50];
 
-ex214_216(t_, x_t, T0, m_max, m_max_i);
+ex214_216(t_, x_t, T0, m_max, m_max_i, 'Ex. 2.2.3: ');
 
 %% Ex. 2.2.4
-T0 = 2 * pi;
+% x_t	= -2 + 4 * cos(4 * t_ + pi / 3) - 2 * sin(10 * t_)
+%		= 2 * cos(pi) + 4 * cos(4 * t_ + pi / 3) + 2 * cos(10 * t_ + pi / 2)
+%
+% W0	= 2
+% T0	= 2 * pi / W0 = pi;
+
+T0 = pi;
+
 t_ = linspace(0, T0, 500);
-x_t = -2 + 4 * cos(4 * t_ + pi / 3) - 2 * sin(10 * t_);
+x_t = 2 * cos(pi) + 4 * cos(4 * t_ + pi / 3) + 2 * cos(10 * t_ + pi / 2);
 
 m_max = 100;
 m_max_i = [0, 1, 3, 5, 10, 50];
 
-ex214_216(t_, x_t, T0, m_max, m_max_i);
+ex214_216(t_, x_t, T0, m_max, m_max_i, 'Ex. 2.2.4: ');
--- tp2/src/ex214_216.m
+++ tp2/src/ex214_216.m
@@ -1,36 +1,33 @@
-function [] = ex214_216(t_, x_t, T0, m_max, m_max_i)
+function ex214_216(t_, x_t, T0, m_max, m_max_i, title_prefix)
 %% Ex. 2.1.1
 W0 = 2 * pi / T0;
 
 %% Ex. 2.1.4
-fprintf('Ex. 2.1.4\n');
+fprintf('%s (analog to 2.1.4)\n', title_prefix);
 
 [C_m, Theta_m] = FourierSeries(t_', x_t', T0, m_max);
 
-titl_ = 'Fourier Transform %s, for m = %d';
-figure('Name', sprintf('Ex. 2.1.4: %s', sprintf(titl_, 'Coefficient (Cm) and Sinusoidal Phase, θm', m_max)));
+titl_ = 'Trigonometric Fourier Series %s, for %s = %d';
+figure('Name', sprintf('%s%s', title_prefix, sprintf(titl_, 'Coefficient (Cm) and Sinusoidal Phase, θm', 'm_max', m_max)));
 subplot(2, 1, 1);
 stem(0:m_max, C_m);
-title(sprintf(titl_, 'Coefficient, C_m', m_max));
+title(sprintf(titl_, 'Coefficient, C_m', 'm\_max', m_max));
 
 subplot(2, 1, 2);
 stem(0:m_max, Theta_m);
-title(sprintf(titl_, 'Sinusoidal Phase, \theta_m', m_max));
+title(sprintf(titl_, 'Sinusoidal Phase, \theta_m', 'm\_max', m_max));
 fprintf('Press ENTER to continue.\n\n'); pause();
 
 %% Ex. 2.1.5
-fprintf('Ex. 2.1.5 (plotted)\n');
+fprintf('%s (analog to 2.1.5, plotted)\n', title_prefix);
 
 interval = '';
 for m = 1:length(m_max_i)
-	interval = sprintf('%s%d', interval, m_max_i(m));
-
-	if m ~= length(m_max_i)
-		interval = sprintf('%s, ', interval);
-	end
+	interval = sprintf('%s%d, ', interval, m_max_i(m));
 end
+interval = sprintf('%s%d', interval, m_max);
 
-titl_ = sprintf('Fourier Transform Signal Approximation for m %%s [%s]', interval);
+titl_ = sprintf('%s%s', title_prefix, sprintf('Fourier Series Signal Approximation for m %%s [%s]', interval));
 figure('Name', sprintf(titl_, '∈'));
 hold all;
 
@@ -38,7 +35,7 @@ plot(t_, x_t, 'b'); % original signal
 
 i_ = 1;
 ft = zeros(size(t_));
-plot_colors = ['r', 'g', 'm', 'k'];
+plot_colors = ['r', 'g', 'm', 'c'];
 for m = 0:m_max
 	ft = ft + C_m(m+1) * cos(m * W0 * t_ + Theta_m(m+1));
 
@@ -48,15 +45,15 @@ for m = 0:m_max
 	end
 end
 
-plot(t_, ft, plot_colors(mod(length(m_max_i), length(plot_colors)) + 1));
-plot(t_, x_t, 'b--'); % original signal (to assure it is visible as well as a perfect approximation)
+plot(t_, ft, 'r'); % m_max always plotted red
+plot(t_, x_t, 'b--'); % original signal always plotted blue (second plotting to assure it is visible as well as a perfect approximation)
 title(sprintf(titl_, '\in'));
 hold off;
 
 fprintf('Press ENTER to continue.\n\n'); pause();
 
 %% Ex. 2.1.6
-fprintf('Ex. 2.1.6 (plotted)\n');
+fprintf('%s (analog to 2.1.6, plotted)\n', title_prefix);
 c_m = zeros(1, m_max * 2 + 1);
 c_m(m_max + 1) = C_m(1) * cos(Theta_m(1));
 
@@ -68,8 +65,8 @@ end
 c_m_amp = abs(c_m);
 c_m_phase = angle(c_m);
 
-titl_ = 'Fourier Transform %s coefficients (%s), for m %s [%d, %d]';
-figure('Name', sprintf('Ex: 2.1.6: %s', sprintf(titl_, 'cm', 'amplitude and phase', '∈', -m_max, m_max)));
+titl_ = 'Complex Fourier Series %s coefficients (%s), for m %s [%d, %d]';
+figure('Name', sprintf('%s%s', title_prefix, sprintf(titl_, 'cm', 'amplitude and phase', '∈', -m_max, m_max)));
 subplot(2, 1, 1);
 plot(-m_max:m_max, c_m_amp, '.');
 title(sprintf(titl_, 'c_m', 'amplitude', '\in', -m_max, m_max));
